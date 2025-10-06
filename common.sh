2b3246d155c674e3cd6311732a6b24e0d90dca24
TulasiLatha <lathagangadhari98@gmail.com>
2025-10-06 17:08:37 +0530

    session-6

--- common.sh
+++ common.sh
@@ -10,6 +10,7 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 START_TIME=$(date +%s)
+SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.tulasidevops.fun
 
 mkdir -p $LOGS_FOLDER
